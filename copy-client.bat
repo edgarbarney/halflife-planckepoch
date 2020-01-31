0bad25da089fb499e90457ff6ec10b643f19cfa9
Daniel Walder <daniel.walder@gmail.com>
2020-01-31 17:28:57 +1000

    Add copy batch files as post-build steps

---
+++ copy-client.bat
@@ -0,0 +1,2 @@
+rem Put a copy command in here to copy your client.dll into your mod directory
+robocopy ".\projects\vs2019\Debug\hl_cdll" "path-to-your-mod-folder\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
---
+++ copy-server.bat
@@ -0,0 +1,2 @@
+rem Put a copy command in here to copy your client.dll into your mod directory
+robocopy ".\projects\vs2019\Debug\hldll" "path-to-your-mod-folder\dlls" hl.dll /njh /njs /ndl /nc /ns /np
