61510fbca342ffef62825938e15d745eaedbd096
FranticDreamer <edwardgmgmd@gmail.com>
2020-11-24 15:22:26 +0300

    Revert ignoring copy.bat files

--- copy-client.bat
+++ copy-client.bat
@@ -1,2 +1,2 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-robocopy ".\projects\vs2019\Debug\hl_cdll" "path-to-your-mod-folder\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hl_cdll" "E:\STEAMFILES\steamapps\common\Half-Life\edmd\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
