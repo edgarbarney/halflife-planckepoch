ea9e92c3ed45e3d36539f4e109e823b4173f06d0
FranticDreamer <edwardgmgmd@gmail.com>
2021-02-26 18:23:07 +0300

    DEV -  Copy Path Fix

--- copy-server.bat
+++ copy-server.bat
@@ -1,2 +1,3 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-rem robocopy ".\projects\vs2019\Debug\hldll" "path-to-your-mod-folder\dlls" hl.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\SpiritTrinity\dlls" hl.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\SpiritTrinity\dlls" hl.pdb /njh /njs /ndl /nc /ns /np
