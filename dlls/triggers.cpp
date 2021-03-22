c4a0592e7f2e8dc3e2f50cb27475d5990ea71132
Daniel Walder <daniel.walder@gmail.com>
2021-03-22 18:58:03 +1000

    Updates to support Linux build

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -34,6 +34,8 @@
 //#include "hgrunt.h"
 //#include "islave.h"
 
+#include <cctype>
+
 #define	SF_TRIGGER_PUSH_START_OFF	2//spawnflag that makes trigger_push spawn turned OFF
 #define SF_TRIGGER_HURT_TARGETONCE	1// Only fire hurt target once
 #define	SF_TRIGGER_HURT_START_OFF	2//spawnflag that makes trigger_hurt spawn turned OFF
