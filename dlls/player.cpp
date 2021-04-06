87f98239ae873ccff16138880a366fe7b4918678
FranticDreamer <edwardgmgmd@gmail.com>
2021-04-06 11:03:25 +0300

    Fix a crash of SwitchWeapon
    When SwitchWeapon's "pWeapon" argument is nullptr, try to switch to last item. If last item is nullptr too, don't do anything. At least don't crash.

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -5215,8 +5215,16 @@ BOOL CBasePlayer :: SwitchWeapon( CBasePlayerItem *pWeapon )
 		m_pActiveItem->UpdateItemInfo( );
 	}
 #else
-	m_pActiveItem = pWeapon;
-	pWeapon->Deploy( );
+	if (pWeapon != nullptr)
+	{
+		m_pActiveItem = pWeapon;
+		pWeapon->Deploy();
+	}
+	else if (m_pLastItem != nullptr)
+	{
+		m_pActiveItem = m_pLastItem;
+		m_pLastItem->Deploy();
+	}
 #endif
 	return TRUE;
 }
