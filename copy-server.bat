f0351e6959ab34bd5b6ed9514c0b824429d68a6f
FranticDreamer <edwardgmgmd@gmail.com>
2021-04-19 13:34:30 +0300

    Revert "DEV - Copy Path Fix"
    This reverts commit 08a2ec322aab9764349cb33d92aa4e6b5d48a1c2.

--- copy-server.bat
+++ copy-server.bat
@@ -1,3 +1,3 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\spirinop4\dlls" opfor.dll /njh /njs /ndl /nc /ns /np
-robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\spirinop4\dlls" opfor.pdb /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\SpiritTrinity\dlls" spirit.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\SpiritTrinity\dlls" spirit.pdb /njh /njs /ndl /nc /ns /np
