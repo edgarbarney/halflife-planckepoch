816f4c04e3542b0475ad430cb60d423d30a1bdd3
FranticDreamer <edwardgmgmd@gmail.com>
2021-08-27 17:51:24 +0300

    DEV - Change Debug Copy Filename

--- copy-server.bat
+++ copy-server.bat
@@ -1,3 +1,3 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\dlls" spirit.dll /njh /njs /ndl /nc /ns /np
-robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\dlls" spirit.pdb /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\dlls" admurr.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hldll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\dlls" admurr.pdb /njh /njs /ndl /nc /ns /np
