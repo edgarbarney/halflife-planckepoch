9dc85b276f70fd962208f9a58c71a9fa972df502
FranticDreamer <edwardgmgmd@gmail.com>
2021-08-27 17:11:41 +0300

    DEV - Change Debug Copy Directory

--- copy-client.bat
+++ copy-client.bat
@@ -1,3 +1,3 @@
 rem Put a copy command in here to copy your client.dll into your mod directory
-robocopy ".\projects\vs2019\Debug\hl_cdll" "E:\STEAMFILES\steamapps\common\Half-Life\spirinityop4\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
-robocopy ".\projects\vs2019\Debug\hl_cdll" "E:\STEAMFILES\steamapps\common\Half-Life\spirinityop4\cl_dlls" client.pdb /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hl_cdll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\cl_dlls" client.dll /njh /njs /ndl /nc /ns /np
+robocopy ".\projects\vs2019\Debug\hl_cdll" "E:\STEAMFILES\steamapps\common\Half-Life\AdmerInvasion\cl_dlls" client.pdb /njh /njs /ndl /nc /ns /np
